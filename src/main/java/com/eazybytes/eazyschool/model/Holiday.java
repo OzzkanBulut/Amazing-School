f178e5ffb226848293aa227c8dce49d4eb2df031
OzzkanBulut <ozkanb66@gmail.com>
2024-07-29 23:32:13 +0300

    Holidays added to db

--- src/main/java/com/eazybytes/eazyschool/model/Holiday.java
+++ src/main/java/com/eazybytes/eazyschool/model/Holiday.java
@@ -3,11 +3,18 @@ package com.eazybytes.eazyschool.model;
 import lombok.Data;
 
 @Data
-public class Holiday {
+public class Holiday extends BaseEntity {
 
-    private final String day;
-    private final String reason;
-    private final Type type;
+    private String day;
+    private String reason;
+    private Type type;
+
+    public Holiday(String s, String s1, Type type) {
+        day = s;
+        reason = s1;
+        this.type = type;
+    }
+
 
     public enum Type {
         FESTIVAL, FEDERAL
