5c9c066755941be831bb20d33c9ce24c93bd53c0
OzzkanBulut <ozkanb66@gmail.com>
2024-08-25 21:34:09 +0300

    Class and students crud

--- src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
+++ src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
@@ -3,11 +3,13 @@ package com.eazybytes.eazyschool.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "class")
 public class EazyClass extends BaseEntity {
