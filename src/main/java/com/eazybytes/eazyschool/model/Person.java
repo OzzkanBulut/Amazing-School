890771941ce6bc2c6c19447d11282c68982f0296
OzzkanBulut <ozkanb66@gmail.com>
2024-09-01 13:58:48 +0300

    Courses, classes ,1to1,Manyto1, a lot of stuff

--- src/main/java/com/eazybytes/eazyschool/model/Person.java
+++ src/main/java/com/eazybytes/eazyschool/model/Person.java
@@ -10,6 +10,9 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
 @Entity
@@ -69,6 +72,16 @@ public class Person extends BaseEntity{
     @ManyToOne(fetch = FetchType.LAZY,optional = true)
     @JoinColumn(name = "class_id",referencedColumnName = "classId",nullable = true)
     private EazyClass eazyClass;
+
+    @ManyToMany(cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
+    @JoinTable(name = "person_courses",
+            joinColumns ={
+            @JoinColumn(name ="person_id",referencedColumnName = "personId")
+            },
+            inverseJoinColumns ={
+            @JoinColumn(name = "course_id",referencedColumnName = "courseId")
+            } )
+    private Set<Courses> courses = new HashSet<>();
 
 
 }
